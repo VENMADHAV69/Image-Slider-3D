2252481ef7519a7f093e77361c92c5914193ea9a
I VENU MADHAV <venu05277@gmail.com>
2024-11-03 19:58:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ imagesData.forEach((img, index) => {
 
          // Create and append image element to slide item
 	const imageElement = document.createElement("img");
-	imageElement.src = `https://picsum.photos/200/300`;
+	imageElement.src = `https://picsum.photos/600/700`;
 	imageElement.alt = img.alt;
 	liElm.appendChild(imageElement);
 
